7fcdbb9f12b7a64f6153d8bfbbc29b5cd0ad1c66
keith2018 <keith@robot9.me>
2022-06-03 08:42:05 +0800

    update source file names

---
+++ src/app/environment.h
@@ -0,0 +1,35 @@
+/*
+ * SoftGLRender
+ * @author 	: keith@robot9.me
+ *
+ */
+
+#pragma once
+
+#include "camera.h"
+#include "renderer/texture.h"
+#include "renderer/renderer.h"
+
+namespace SoftGL {
+namespace App {
+
+class Environment {
+ public:
+  static void ConvertEquirectangular(Texture &tex_in, Texture *tex_out, int width = 0, int height = 0);
+
+  static void GenerateIrradianceMap(Texture *tex_in, Texture *tex_out);
+
+  static void GeneratePrefilterMap(Texture *tex_in, Texture *tex_out);
+
+ private:
+  static void GeneratePrefilterMapLod(Texture *tex_in, Texture *tex_out, int width, int height, int mip);
+
+  static std::shared_ptr<Renderer> CreateCubeRender(Camera &camera, int width, int height);
+
+  static void CubeRenderDraw(Renderer &renderer,
+                             Camera &camera,
+                             const std::function<void(int face, Buffer<glm::u8vec4> &buff)> &face_cb);
+};
+
+}
+}
